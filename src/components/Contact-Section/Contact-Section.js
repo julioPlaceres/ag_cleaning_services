7065ea76d8f61d6b6791351b3ec36700852de168
dreamheart <leondreamheart@outlook.com>
2022-06-22 00:04:12 -0400

    Added hours of operation to the contact-section for the footer, need styling

--- src/components/Contact-Section/Contact-Section.js
+++ src/components/Contact-Section/Contact-Section.js
@@ -1,21 +1,60 @@
 import "./Contact-Section.css";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 const ContactSection = () => {
   return (
-      <div>
-        <hr/>
+    <Container fluid>
+      <Row className="sepration-lines">
+        <hr style={{ width: "85%" }} />
+      </Row>
 
-          <h6>AG Organizing &amp; Cleaning Services LLC</h6>
+      <Row className="titles container">
+        <h6>AG Organizing &amp; Cleaning Services LLC</h6>
+      </Row>
 
-          <p>Hours of Operation</p>
+      <Row className="titles container">
+        <p>Hours of Operation</p>
+      </Row>
 
-          <hr style={{ width: '50%'}}/>
+      <Row className="sepration-lines">
+        <hr style={{ width: "50%", color: "green", height: "2px" }} />
+      </Row>
 
-          <ul>
-            <li>Monday: 9:00am - 5:00pm</li>
-            <li>Tuesday: 9:00am - 5:00pm</li>
-          </ul>
-      </div>
+      <Row className="container">
+        <Table borderless>
+          <tr>
+            <td>Sunday</td>
+            <td>Closed</td>
+          </tr>
+          <tr>
+            <td>Monday</td>
+            <td>9:00 AM - 5:00 PM</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>9:00 AM - 5:00 PM</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>9:00 AM - 5:00 PM</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>9:30 AM - 8:00 PM</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>9:30 AM - 8:00 PM</td>
+          </tr>
+          <tr>
+            <td>Saturday</td>
+            <td>10:00 AM - 6:00 PM</td>
+          </tr>
+        </Table>
+      </Row>
+    </Container>
   );
 };
 
