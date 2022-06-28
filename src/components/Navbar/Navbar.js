c1522ed54a0b3a5d910a32e2eae46a76aa236fc8
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-06-28 16:04:01 -0400

    adding animate-nav as a className for the styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,17 +3,17 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 function Navigation() {
   return (
     <>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg"  className="animate-nav" >
         <Container>
           <Navbar.Brand href="/LogoImg.jpg"></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/home">Home</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/services">Services</Nav.Link>
-              <Nav.Link href="/reviews">Reviews</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link className="styling" href="/home">Home</Nav.Link>
+              <Nav.Link className="styling" href="/about">About</Nav.Link>
+              <Nav.Link className="styling" href="/services">Services</Nav.Link>
+              <Nav.Link  className="styling" href="/reviews">Reviews</Nav.Link>
+              <Nav.Link className="styling"href="/contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
