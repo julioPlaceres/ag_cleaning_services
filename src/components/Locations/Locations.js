9fd1886cc559ec5fecbc62fd28212c07cf004544
dreamheart <leondreamheart@outlook.com>
2022-06-27 20:46:32 -0400

    Created component for locations.

---
+++ src/components/Locations/Locations.js
@@ -0,0 +1,42 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./Locations.css";
+
+function Locations() {
+  return (
+    <>
+      <Container className="mt-4 container">
+        <Row className="locations">
+          <Col>
+            <ul>
+              <li>Liverpool, NY</li>
+              <li>Cazenovia, NY</li>
+              <li>Central Square, NY</li>
+              <li>Camillus, NY</li>
+              <li>Brewerton, NY</li>
+              <li>Fairmount, NY</li>
+              <li>Manlius, NY</li>
+              <li>Cicero, NY</li>
+              <li>Clay, NY</li>
+            </ul>
+          </Col>
+          <Col>
+            <ul>
+              <li>Syracuse, NY</li>
+              <li>Fayetteville, NY</li>
+              <li>Baldwinsville, NY</li>
+              <li>North Syracuse, NY</li>
+              <li>Bridgeport, NY</li>
+              <li>Kirkville, NY</li>
+              <li>Chittenango, NY</li>
+              <li>Lakeport, NY</li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Locations;
