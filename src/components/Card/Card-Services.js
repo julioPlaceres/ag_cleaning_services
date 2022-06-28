ff8422c0cd77ec19936d5ee1a8656a5e8d970a44
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-06-28 16:05:22 -0400

    changing the font color as black and intro as the classname for the styling

--- src/components/Card/Card-Services.js
+++ src/components/Card/Card-Services.js
@@ -3,18 +3,18 @@ import { Card, Button } from "react-bootstrap";
 
 const CardServices = (props) => {
   return (
-<Card border="light" style={{ width: "20rem", borderRadius: "50px", maxHeight: "600px"}} className="mx-auto"> 
+<Card border="light" style={{ width: "20rem", borderRadius: "10px", maxHeight: "600px", height: "500px"}} className="mx-auto"> 
     
     <Card.Img variant="top" 
     src={props.src} 
     alt="card image" 
-    style={{maxWidth: "300px"}} 
-    className="mx-auto"/>
+    style={{maxWidth: "400px"}}
+    />
       
       <Card.Body>
         <Card.Title style={{color: "black"}}>{props.title}</Card.Title>
         <Card.Text style={{color: "black"}}> <small>{props.text}</small></Card.Text>
-        <Button variant="info">{props.btnText}</Button>
+        <Button className="button" variant="info">{props.btnText}</Button>
       </Card.Body>
       
 </Card>
