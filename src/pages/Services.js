26dc72ba1a6c8fe2bd33f6a7a66f9813e7b3ab5b
rlaquea <rlaquea14@gmail.com>
2022-07-05 21:27:09 -0400

    added services available list, background and container

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,7 +1,32 @@
+import { Container } from "react-bootstrap";
+
 function Services() {
     return (
       <>
-      <h1>What better than the fresh scent of a clean house!</h1>
+      <h1>Services</h1>
+      <h3 style={{margin: "5%"}}>Here's a comprehensive list of what we offer:</h3>
+      <p>
+        
+        <Container className="services-list" style={{border: "3px solid gray", background: "lightblue", fontSize: "24px", font: "Calibri bold", margin: "0%", padding: "5%" }}>
+        <tbody>
+        <tr>Standard cleaning</tr>
+        <tr>Deep cleaning of rooms, including garages. </tr>
+        <tr>Moving-related cleaning </tr>
+        <tr>Office and workplace cleaning </tr>
+        <tr>Vacation rental and Airbnb cleaning</tr>
+        <tr>After construction cleaning</tr>
+        <tr>Apartment cleaning</tr>
+        <tr>Wash, dry and fold laundry</tr>
+
+        <tr>Cleaning areas include:</tr>
+        <tr>Closet, attic, garage, basement and cabinet cleaning/organization</tr>
+        <tr>Interior window cleaning</tr>
+        <tr>Inside oven, refrigerator, microwave, sub-zero</tr>
+        <tr>Baseboards</tr>
+        </tbody>
+
+        </Container>
+      </p>
       </>
     );
   }
