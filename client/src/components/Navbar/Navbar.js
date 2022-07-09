b520d0e19ac89dc5ad9b4fea905bef182b7ed80f
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-07-08 21:47:19 -0400

    adding a classname styling to style the component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -11,11 +11,21 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/services">Services</Nav.Link>
-            <Nav.Link href="/reviews">Reviews</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link className="styling" href="/home">
+              Home
+            </Nav.Link>
+            <Nav.Link className="styling" href="/about">
+              About
+            </Nav.Link>
+            <Nav.Link className="styling" href="/services">
+              Services
+            </Nav.Link>
+            <Nav.Link className="styling" href="/reviews">
+              Reviews
+            </Nav.Link>
+            <Nav.Link className="styling" href="/contact">
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
