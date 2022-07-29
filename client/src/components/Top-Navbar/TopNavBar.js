5b0dd9178e72ea70deb45a02d061df9388b05ae4
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-07-29 17:06:06 -0400

    add updating navbar

--- client/src/components/Top-Navbar/TopNavBar.js
+++ client/src/components/Top-Navbar/TopNavBar.js
@@ -9,19 +9,19 @@ const TopNavBar = () => {
   return (
     <Container className="background" fluid>
       <Row className="icon-row">
-        <Col xs={6} md={3} lg={2} className="phone">
+        <Col xs={12} md={3} lg={2} className="phone">
           <PhoneIcon />
         </Col>
 
-        <Col xs={6} md={5} lg={3} className="email">
+        <Col xs={12} md={3} lg={3} className="email">
           <EmailIcon />
         </Col>
 
-        <Col xs={3} className="facebook icon">
+        <Col xs={12} md={3} lg={3} className="facebook icon">
           <Facebook />
         </Col>
 
-        <Col xs={2} className="instagram icon">
+        <Col xs={12} md={3} lg={3} className="instagram icon">
           <Instagram />
         </Col>
       </Row>
