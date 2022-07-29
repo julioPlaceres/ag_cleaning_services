d368d08890be3f0820812393a113cf05b2e813d8
dreamheart <leondreamheart@outlook.com>
2022-07-28 21:52:21 -0400

    Joined top navbar and navigation components into one

--- client/src/components/Link-Icons/Facebook.js
+++ client/src/components/Link-Icons/Facebook.js
@@ -1,10 +1,10 @@
-import "./Link-Icons.css";
-import { MdFacebook } from "react-icons/md";
+import './Link-Icons.css';
+import { MdFacebook } from 'react-icons/md';
 
 const Facebok = () => {
   return (
     <a
-      className="link-icons-altern"
+      className="link-icons-altern icon"
       target="_bank"
       href="https://www.facebook.com/AG_OCleaning-services-LLC-101415875788474"
     >
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,37 +1,51 @@
 import './Navbar.css';
-import { Navbar, Nav } from 'react-bootstrap';
+import EmailIcon from '../Link-Icons/Email';
+import PhoneIcon from '../Link-Icons/Phone';
+import Facebook from '../Link-Icons/Facebook';
+import Instagram from '../Link-Icons/Instagram';
+import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 
 // To what are those classes link to?
 function Navigation() {
   return (
-    <>
-      <Navbar expand="lg" className="animate-nav">
-        <Navbar.Brand href="/home">
-          {/* Show Logo on Navbar only if not on Home Page */}
-          {/* <img src={Logo} width="100" height="70" /> */}
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link className="styling" href="/home">
-              Home
-            </Nav.Link>
-            <Nav.Link className="styling" href="/about">
-              About
-            </Nav.Link>
-            <Nav.Link className="styling" href="/services">
-              Services
-            </Nav.Link>
-            <Nav.Link className="styling" href="/reviews">
-              Reviews
-            </Nav.Link>
-            <Nav.Link className="styling" href="/contact">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
+    <Container fluid>
+      <Row>
+        <Col xs={6}>
+          <PhoneIcon />
+        </Col>
+
+        <Col xs={6} className="email">
+          <EmailIcon />
+        </Col>
+
+        <Col xs={3}>
+          <Facebook />
+        </Col>
+
+        <Col xs={3}>
+          <Instagram />
+        </Col>
+
+        <Col xs={6}>
+          <Navbar expand="lg">
+            <Navbar.Brand href="/home">
+              {/* Show Logo on Navbar only if not on Home Page */}
+              {/* <img src={Logo} width="100" height="70" /> */}
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="">
+                <Nav.Link href="/home">Home</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/services">Services</Nav.Link>
+                <Nav.Link href="/reviews">Reviews</Nav.Link>
+                <Nav.Link href="/contact">Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
