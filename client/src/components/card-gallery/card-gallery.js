c564a070443b045d2a885fb7c58dd534ba18d76b
rlaquea <rlaquea14@gmail.com>
2022-08-25 23:01:12 -0400

    adjusted image vw using Image component

--- client/src/components/card-gallery/card-gallery.js
+++ client/src/components/card-gallery/card-gallery.js
@@ -1,22 +1,10 @@
 import './card-gallery.css';
-import { Link } from 'react-router-dom';
-import { Container, Card } from 'react-bootstrap';
-
-// This is for selecting a single image (TODO)
-function CardItem(cardItem) {
-  const { _id, name, image } = cardItem;
-
-  return (
-    <div>
-      <Link to={`/gallery/${_id}`}></Link>
-    </div>
-  );
-}
+import Image from 'react-bootstrap/Image';
 
 const GalleryCard = (props) => {
   return(
     <>
-      <img width="200" src={props.src}></img>
+    <Image width="100vw" src={props.src}/>
     </>
   )
 }
