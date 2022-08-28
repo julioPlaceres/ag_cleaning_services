3c98b965afc6f6cf7203ffa3c8ec1fe3daa2fc51
dreamheart <leondreamheart@outlook.com>
2022-08-28 16:32:58 -0400

    Resolved github issues

--- client/src/components/card-gallery/card-gallery.js
+++ client/src/components/card-gallery/card-gallery.js
@@ -1,20 +1,6 @@
 import './card-gallery.css';
 import Image from 'react-bootstrap/Image';
 
-<<<<<<< HEAD
-const CardItem = (props) => {
-  const { _id, name, image } = props;
-
-  return (
-    <div className="card px-1 py-1">
-      <Link to={`/gallery/${_id}`}>
-        <img alt={name} src={`/images/${image}`} />
-        <p>{name}</p>
-      </Link>
-    </div>
-  );
-};
-=======
 const GalleryCard = (props) => {
   return(
     <>
@@ -22,6 +8,5 @@ const GalleryCard = (props) => {
     </>
   )
 }
->>>>>>> 7220d807219c224eebedf8b9bfc85f74e7c51593
 
 export default GalleryCard;
