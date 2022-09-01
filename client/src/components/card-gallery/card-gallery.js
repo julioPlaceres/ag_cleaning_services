26c309f4a77f632a62ac09533d58ec8d21d79fee
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-09-01 19:08:16 -0400

    trying out different properties

--- client/src/components/card-gallery/card-gallery.js
+++ client/src/components/card-gallery/card-gallery.js
@@ -2,11 +2,11 @@ import './card-gallery.css';
 import Image from 'react-bootstrap/Image';
 
 const GalleryCard = (props) => {
-  return(
+  return (
     <>
-    <Image width="119.5vw" src={props.src}/>
+      <Image width="119.5vw" src={props.src} />
     </>
-  )
-}
+  );
+};
 
 export default GalleryCard;
