36ff58a06090939f8b225a5dca9e2a3173769db9
julioPlaceres <leondreamheart@outlook.com>
2022-09-01 13:10:41 -0400

    Added toogle button and logic for the navbar icon

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,44 +3,47 @@ import EmailIcon from '../Link-Icons/Email';
 import PhoneIcon from '../Link-Icons/Phone';
 import Facebook from '../Link-Icons/Facebook';
 import Instagram from '../Link-Icons/Instagram';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { AiOutlineClose } from 'react-icons/ai';
 import { Container, Row, Col, Navbar, Nav } from 'react-bootstrap';
 
-// To what are those classes link to?
 function Navigation() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
-    <Container fluid>
-      <Row>
-        <Col xs={6}>
-          <PhoneIcon />
-        </Col>
+    <>
+      <Navbar expand="lg">
+        <Container fluid>
+          <Col xs={6} sm={6} md={2} className="phone">
+            <PhoneIcon />
+          </Col>
 
-        <Col xs={6} className="email">
-          <EmailIcon />
-        </Col>
+          <Col xs={6} sm={6} md={2} className="email">
+            <EmailIcon />
+          </Col>
 
-        <Col xs={1}>
-          <Facebook />
-        </Col>
+          <Row xs={6}>
+            <Col>
+              <Facebook />
+            </Col>
+            <Col>
+              <Instagram />
+            </Col>
+          </Row>
 
-        <Col xs={1}>
-          <Instagram />
-        </Col>
-        
-        <Col xs={6}>
-          <Navbar
-            collapseOnSelect
-            fixed="top"
-            expand="lg"
-            bg="secondary"
-            variant="dark"
-            className="Navbar-Styling"
-          >
-            <Navbar.Brand href="/home">
-              {/* Show Logo on Navbar only if not on Home Page */}
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Col xs="auto">
+            <Navbar.Toggle
+              aria-controls="responsive-navbar-nav"
+              onClick={handleClick}
+              children={click ? <AiOutlineClose /> : <GiHamburgerMenu />}
+            />
+          </Col>
+
+          <Col lg={6}>
             <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav className="">
+              <Nav>
                 <Nav.Link href="/home">Home</Nav.Link>
                 <Nav.Link href="/about">About</Nav.Link>
                 <Nav.Link href="/gallery">Gallery</Nav.Link>
@@ -49,10 +52,10 @@ function Navigation() {
                 <Nav.Link href="/contact">Contact</Nav.Link>
               </Nav>
             </Navbar.Collapse>
-          </Navbar>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
