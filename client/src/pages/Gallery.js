a5dca0ee99795469c9d030bc6b52a5bb5ad8d3eb
rlaquea <rlaquea14@gmail.com>
2022-09-04 19:40:29 -0400

    added try catch statement and url link to gallery.js file

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import GalleryCard from '../components/card-gallery/card-gallery';
+// import urlLink from '../components/card-gallery/card-gallery';
 
 const Gallery = () => {
   const [images = [], setImage] = useState([]);
@@ -10,13 +11,17 @@ const Gallery = () => {
   }, []);
 
   const fetchImages = () => {
-  const galleryOptions = {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json' }
-}
-   fetch("api/gallery", galleryOptions)
-   .then((res) => res.json())
-//catch & log catch
+    const galleryOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    }
+    fetch("api/gallery", galleryOptions)
+      .then((res) => res.json())
+      // try {
+      //   console.log(images, galleryOptions)
+      // } catch (error) {
+      //   console.log(`Error: ${error}`)
+      // }
   };
 
   return (
