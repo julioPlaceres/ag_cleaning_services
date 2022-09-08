9ea164341be34ee1f72f33e57ddb30aec6b12334
wilmerojeda13 <ojedawilmerantonio@gmail.com>
2022-09-07 20:58:14 -0400

    trying out different properties

--- client/src/components/Hero-Section/Hero-Section.js
+++ client/src/components/Hero-Section/Hero-Section.js
@@ -1,6 +1,6 @@
-import "./Hero-Section.css";
-import Img1 from "../images/heroImg.jpg";
-import { Container, Image } from "react-bootstrap";
+import './Hero-Section.css';
+import Img1 from '../images/heroImg.jpg';
+import { Container, Image } from 'react-bootstrap';
 
 const HeroSection = () => {
   return (
