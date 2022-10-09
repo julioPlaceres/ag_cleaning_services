5f1dee7a5ee2d1f6f9bd8d0765e55f647e655d81
dreamheart <leondreamheart@outlook.com>
2022-10-09 15:28:43 -0400

    updated routes for heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 // Prefix all routes defined in the api directory with `/api`
-router.use('/api', apiRoutes);
+router.use('https://vast-stream-13430.herokuapp.com/api', apiRoutes);
 
 module.exports = router;
