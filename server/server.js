5983ae69437a4f5c61fb048b748f00bc139bab29
dreamheart <leondreamheart@outlook.com>
2022-10-09 14:03:21 -0400

    updated static files path

--- server/server.js
+++ server/server.js
@@ -15,18 +15,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(
-//   '/images',
-//   express.static(path.join(__dirname, '../client/src/components/images'))
-// );
-
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.get('*', function (req, res) {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // turn on routes
 app.use(routes);
