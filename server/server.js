15f12506a5761502f8abe5048cfba202b6b27bc7
dreamheart <leondreamheart@outlook.com>
2022-10-09 16:05:16 -0400

    build prod

--- server/server.js
+++ server/server.js
@@ -15,6 +15,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // turn on routes
 app.use(routes);
 
