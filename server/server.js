2be814166bd3a5ae5b809695c63d4b5902d3b2fe
julioPlaceres <leondreamheart@outlook.com>
2022-10-12 12:03:25 -0400

    Updated package json names and removed static paths on server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-const path = require('path');
 require('dotenv').config();
 require('./config/connection');
 const express = require('express');
@@ -17,16 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
-
-if (process.env.NODE_ENV === 'production') {
-  console.log(`path1: ${path.join(__dirname, '../client/build')}`)
-  console.log(`path2: ${path.join(__dirname, '../client/build/index.html')}`)
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}!`);
